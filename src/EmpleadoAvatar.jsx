373bc6854e47ffebbb1290d27e98a846609b1ce1
Victoria Palacio Fernandez <victoriapalaciof@gmail.com>
2022-09-29 19:39:06 -0300

    pudimos maquetar una card y usar los elementos del array para que se cree una nueva de manera dinamica

--- src/EmpleadoAvatar.jsx
+++ src/EmpleadoAvatar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const EmpleadoAvatar = () => {
+const EmpleadoAvatar = ({empleado}) => {
     return (
         <div>
-            <img src="https://d33i2jw1rbe4t6.cloudfront.net/uploads/company/logo/290/medium_64044245-830f-443f-b4dc-99589c07ad27.png" alt="avatar" className='fluid w-50 d-flex justify-content-start' />
+            <img src={empleado.pic} alt="avatar" className="w-50">
+            </img>
         </div>
     );
 };
--- src/EmpleadoRow.jsx
+++ src/EmpleadoRow.jsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import { Col, Container, ListGroup, Row, Badge } from 'react-bootstrap';
-import EmpleadoAvatar from './EmpleadoAvatar';
+import React from "react";
+import { Col, Container, ListGroup, Row, Badge } from "react-bootstrap";
+import EmpleadoAvatar from "./EmpleadoAvatar";
 
-const EmpleadoRow = () => {
-    return (
-        <ListGroup.Item>
-            <Container>
-                <Row>
-                    <Col>
-            <EmpleadoAvatar>
-            </EmpleadoAvatar>
-                    </Col>
-                    <Col>
-                    <p className="fw-bold">titulo</p>
-                    <p>titulo</p>
-                    <Badge pill bg='primary'>titulo</Badge>
-                    </Col>
-                </Row>
-            </Container>
-        </ListGroup.Item>
-    );
+const EmpleadoRow = ({empleado}) => {
+  return (
+    <>
+      <ListGroup.Item>
+        <Container>
+          <Row>
+            <Col>
+              <EmpleadoAvatar empleado={empleado}></EmpleadoAvatar>
+            </Col>
+            <Col>
+              <p className="fw-bold">{empleado.nombre}</p>
+              <p>{empleado.titulo}</p>
+              <Badge pill bg="primary">
+                {empleado.profesion}
+              </Badge>
+            </Col>
+          </Row>
+        </Container>
+      </ListGroup.Item>
+    </>
+  );
 };
 
 export default EmpleadoRow;
